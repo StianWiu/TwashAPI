28c8da44f50b2073194be67ebeefa25764b289c5
Pignuuu <Pignuuu@gmail.com>
2022-03-26 21:08:28 +0100

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ const port = 3001;
 
 const rateLimit = require('express-rate-limit');
 const limiter = rateLimit({
-  windowMs: 1 * 60 * 1000, // 15 minutes
+  // Allow 3 requests every minute as to not exceed twitch limit
+  windowMs: 1 * 60 * 1000, // 1 minute
   max: 3, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
   standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
   legacyHeaders: false, // Disable the `X-RateLimit-*` headers
